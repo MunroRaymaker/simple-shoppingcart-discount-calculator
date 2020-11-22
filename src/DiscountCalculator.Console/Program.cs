a2eff1cc59faafab572f62161245f4799b63c3ac
Jens Rud Nielsen <jenrud@gmail.com>
2020-11-22 18:43:48 +0100

    Scaffolding and unit test.

---
+++ src/DiscountCalculator.Console/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DiscountCalculator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("***************************");
+            System.Console.WriteLine("Simple Discount Calculator.");
+            System.Console.WriteLine("***************************");
+        }
+    }
+
+    public class Product
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+    
+    public interface IShoppingCart
+    {
+        void AddItemToCart(Product product);
+        decimal GetCartTotal();
+    }
+
+    public class ShoppingCart : IShoppingCart
+    {
+        public void AddItemToCart(Product product)
+        {
+        }
+
+        public decimal GetCartTotal()
+        {
+            return 100;
+        }
+    }
+}
---
+++ tests/DiscountCalculator.Test/ShoppingCartTests.cs
@@ -0,0 +1,24 @@
+using System;
+using DiscountCalculator.Console;
+using Xunit;
+
+namespace DiscountCalculator.Test
+{
+    public class ShoppingCartTests
+    {
+        [Fact]
+        public void when_cart_has_products_should_return_total()
+        {
+            // Arrange 
+            var shoppingCart = new ShoppingCart();
+            shoppingCart.AddItemToCart(new Product());
+
+            // Act
+            var actual = shoppingCart.GetCartTotal();
+            decimal expected = 100;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
